2b1e0736951613826e5987a84260f4322c877794
David Araújo <david2araujo5@gmail.com>
2022-12-23 16:48:12 +0000

    Improved manipulation of elements with dependencies between them

--- src/capparser.py
+++ src/capparser.py
@@ -1,5 +1,5 @@
-from classes import classes
-from enums import enums
+import element
+import enums
 import xml.etree.ElementTree as ET
 import inspect
 
@@ -15,20 +15,20 @@ def parse(filePath=None, stringXML=None):
 def _recursiveParser(parent):
     _, _, tagName = parent.tag.rpartition('}')
     obj = None
-    if(tagName == str(classes.Alert())):
-        obj = classes.Alert()
-    elif(tagName == str(classes.Info())):
-        obj = classes.Info()
-    elif(tagName == str(classes.EventCode())):
-        obj = classes.EventCode()
-    elif(tagName == str(classes.Parameter())):
-        obj = classes.Parameter()
-    elif(tagName == str(classes.Resource())):
-        obj = classes.Resource()
-    elif(tagName == str(classes.Area())):
-        obj = classes.Area()
-    elif(tagName == str(classes.Geocode())):
-        obj = classes.Geocode()
+    if(tagName == str(element.Alert())):
+        obj = element.Alert()
+    elif(tagName == str(element.Info())):
+        obj = element.Info()
+    elif(tagName == str(element.EventCode())):
+        obj = element.EventCode()
+    elif(tagName == str(element.Parameter())):
+        obj = element.Parameter()
+    elif(tagName == str(element.Resource())):
+        obj = element.Resource()
+    elif(tagName == str(element.Area())):
+        obj = element.Area()
+    elif(tagName == str(element.Geocode())):
+        obj = element.Geocode()
     else:
         return None
 
@@ -63,13 +63,13 @@ def _recursiveDeparser(obj, outputFilePath=None):
             root.set(attr[0], attr[1][0])
         elif isinstance(attr[1][0], list):
             for item in attr[1][0]:
-                if isinstance(item, classes.Alert) or isinstance(item, classes.Info) or isinstance(item, classes.EventCode) or isinstance(item, classes.Parameter) or isinstance(item, classes.Resource) or isinstance(item, classes.Area) or isinstance(item, classes.Geocode):
+                if isinstance(item, element.Alert) or isinstance(item, element.Info) or isinstance(item, element.EventCode) or isinstance(item, element.Parameter) or isinstance(item, element.Resource) or isinstance(item, element.Area) or isinstance(item, element.Geocode):
                     root.append(_recursiveDeparser(item))
                 else:
                     child = ET.SubElement(root, attr[0])
                     child.text = item
         else:
-            if isinstance(attr[1][0], classes.Alert) or isinstance(attr[1][0], classes.Info) or isinstance(attr[1][0], classes.EventCode) or isinstance(attr[1][0], classes.Parameter) or isinstance(attr[1][0], classes.Resource) or isinstance(attr[1][0], classes.Area) or isinstance(attr[1][0], classes.Geocode):
+            if isinstance(attr[1][0], element.Alert) or isinstance(attr[1][0], element.Info) or isinstance(attr[1][0], element.EventCode) or isinstance(attr[1][0], element.Parameter) or isinstance(attr[1][0], element.Resource) or isinstance(attr[1][0], element.Area) or isinstance(attr[1][0], element.Geocode):
                 root.append(_recursiveDeparser(attr[1][0]))
             else:
                 child = ET.SubElement(root, attr[0])
