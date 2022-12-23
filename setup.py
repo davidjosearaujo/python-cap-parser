bf271d1067b0ced34e587d21bdbabf7c37db297e
David Araújo <david2araujo5@gmail.com>
2022-12-23 19:27:11 +0000

    Update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="capparser",
-    version="0.0.1",
+    version="1.0.0",
     author="David Araújo",
     author_email="david2araujo5@gmail.com",
     description="Common Alerting Protocol (CAP) parser",
